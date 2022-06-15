895d86831b08eef256afbc46fc7028b01ac1427a
João <jvictorprevedello@hotmail.com>
2022-06-15 08:13:07 -0300

    added google fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,6 +35,13 @@ module.exports = {
           }
         ]
       }
-     }
+     },
+     {
+      resolve: `gatsby-plugin-google-fonts`,
+      options:{
+        fonts: [ `Montserrat`, `muli\:400,400i,700,700i`,],
+        display:"swap",
+      },
+     },
   ],
 }
