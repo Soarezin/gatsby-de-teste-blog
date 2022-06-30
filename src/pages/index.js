4abafb0fe7e1f28b79e22490976d7c240c59a970
João <jvictorprevedello@hotmail.com>
2022-06-29 21:59:28 -0300

    install MDB & add navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,14 @@
 import React from "react"
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
+import { MDBAlert, MDBCol, MDBRow } from "mdbreact";
+
+import "../contents/navbar.html";
+
+function navbar(){
+  return <navbar src="../contents/navbar.html" />
+}
 
 export default function Home() {
   return <div>Hello world!</div>
